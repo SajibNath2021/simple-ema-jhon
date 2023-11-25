e102428250f25977dd9a2ec22c4faa6b3fc9dd9d
Sajib Nath <https://github.com/SajibNath2021>
2023-11-25 17:35:06 +0600

    added cart

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Product = (props) => {
     const { img, price, stock, seller, name, ratings, ratingsCount } = props.product;
@@ -13,7 +15,7 @@ const Product = (props) => {
                 <p>Brand: {seller}</p>
                 <p>Rating:{ratings} stars</p>
                 <p>Total Rating:{ratingsCount} User</p>
-                <button onClick={()=>handleAddToCart(props.product)}>Add to Cart</button>
+                <button onClick={()=>handleAddToCart(props.product)}>Add to Cart <FontAwesomeIcon icon={faCartShopping} /></button>
             </div>
 
         </div>
