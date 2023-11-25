91bb037e2422bee940b2b5663be09693aba12ff1
Sajib Nath <https://github.com/SajibNath2021>
2023-11-25 22:57:03 +0600

    local storage data call and show in cart

---
+++ src/component/Cart/Cart.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Cart.css'
+const Cart = ({cart}) => {
+    const {price, stock, shipping} = cart;
+    
+    let totalPrice = 0;
+    let totalShippingCharge = 0
+    let quantity =0
+    for(const product of cart){
+        product.quantity= product.quantity || 1;
+        totalPrice = totalPrice + product.price;
+        totalShippingCharge = totalShippingCharge + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+    let totalTax = totalPrice*5 / 100;
+    let grandTotal = totalPrice + totalShippingCharge + totalTax;
+    return (
+        <div className='cart'>
+            <h2>Order Summary</h2>
+                <p>Selected Item:{quantity} </p>
+                <p>Total Price: ${totalPrice}</p>
+                <p>Total Shipping Charge: ${totalShippingCharge}</p>
+                <p>Tax: ${totalTax.toFixed(2)}</p>
+                <p>Grand total:{grandTotal.toFixed(2)} </p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProduct] = useState([]);
@@ -9,29 +11,53 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProduct(data))
     }, []);
- //Cart
+
+
+    //Cart
     const [cart, setCart] = useState([]);
-    
-    const handleAddToCart=(product)=>{
+
+    const handleAddToCart = (product) => {
         const totalproduct = [...cart, product];
         setCart(totalproduct);
+        addToDb(product.id)
     }
+
+    //get local storage data
+    useEffect(() => {
+        //step1:get id
+        const storedCart = getShoppingCart();
+        const saveCart = [];
+        for (const id in storedCart) {
+            //step:2 get the product by using id
+            const addedProduct = products.find(product => product.id === id);
+            //step 3: get quantity of the product
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                saveCart.push(addedProduct);
+            }
+            console.log(addedProduct);
+        }
+        //step 5: se the cart
+        setCart(saveCart)
+    }, [products]) //dependenci  produts r data change hole call kore aber
+
+
     return (
         <div className='Shop-Container'>
             <div className='Products-Container'>
                 {
                     products.map(data =>
-                        <Product 
-                        key={data.name}
-                        product={data}
-                        handleAddToCart={handleAddToCart}
+                        <Product
+                            key={data.name}
+                            product={data}
+                            handleAddToCart={handleAddToCart}
                         >
                         </Product>)
                 }
             </div>
             <div className='Cart-Container'>
-                <h2>Shopping Cart</h2>
-                <p>Total Product:{cart.length} </p>
+                <Cart cart={cart} key={cart.name}></Cart>
             </div>
         </div>
     );
