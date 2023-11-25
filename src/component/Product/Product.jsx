db1d5475fcc51cb42bc708aabe6a708cc0863d49
Sajib Nath <https://github.com/SajibNath2021>
2023-11-25 17:25:28 +0600

    added cart

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.product);
-    const { img, price, stock, seller, name, rating, ratingCount } = props.product;
-
+    const { img, price, stock, seller, name, ratings, ratingsCount } = props.product;
+    const handleAddToCart= props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
                 <h4>Product Name: {name}</h4>
                 <h3>Price:${price}</h3>
-                <p>Brand:{seller}</p>
-                <p>Rating:{rating} stars</p>
-                <p>Total Rating:{ratingCount}</p>
-                <button>Add to Cart</button>
+                <p>Brand: {seller}</p>
+                <p>Rating:{ratings} stars</p>
+                <p>Total Rating:{ratingsCount} User</p>
+                <button onClick={()=>handleAddToCart(props.product)}>Add to Cart</button>
             </div>
 
         </div>
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -4,20 +4,34 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProduct] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProduct(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, []);
+ //Cart
+    const [cart, setCart] = useState([]);
+    
+    const handleAddToCart=(product)=>{
+        const totalproduct = [...cart, product];
+        setCart(totalproduct);
+    }
     return (
         <div className='Shop-Container'>
             <div className='Products-Container'>
                 {
-                    products.map(data=> <Product product={data}></Product>)
+                    products.map(data =>
+                        <Product 
+                        key={data.name}
+                        product={data}
+                        handleAddToCart={handleAddToCart}
+                        >
+                        </Product>)
                 }
             </div>
             <div className='Cart-Container'>
                 <h2>Shopping Cart</h2>
+                <p>Total Product:{cart.length} </p>
             </div>
         </div>
     );
